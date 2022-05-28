4a700c2e4e5b6675571cae5d8c4274129d2e761f
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2022-05-28 13:46:47 +0200

    Aufgabe 3.5 fill Student-class

--- DHStudent.java
+++ DHStudent.java
@@ -1,3 +1,20 @@
-public class DHStudent {
-    
+public class DHStudent extends Student {
+    private String kurs;
+
+    public DHStudent(String name, String kurs) {
+        super(name);
+        this.kurs = kurs;
+    }
+
+    void druckeKurs() {
+        System.out.println("Der DHStudent " + super.getName() + " ist im " + this.kurs + " Kurs.");
+    }
+
+    public void arbeitenOhneMurren() {
+        System.out.println("Ich bin der DHStudent " + this.name + " und gehe daher immer freudig meine Arbeit an!");
+    }
+
+    public void druckeObjekt() {
+        super.druckeObjekt(true);
+    }
 }
--- Student.java
+++ Student.java
@@ -1,3 +1,47 @@
-public class Student {
-    
+import java.util.Random;
+
+public class Student extends Person implements IKlausurSchreiber {
+    int semester;
+    private double bafoegSchulden;
+
+    public Student(String name) {
+        super(name);
+        Random rand = new Random();
+        if (this instanceof DHStudent){
+            this.semester = rand.nextInt(6) + 1;
+            this.bafoegSchulden = 0.0;
+        } else {
+            this.semester = rand.nextInt(12) + 1;
+            this.bafoegSchulden = semester * 3000.00;
+        }
+    }
+
+    public void druckeSemester() {
+        System.out.println("Der Student " + super.getName() + " ist im " + this.semester + ". Semester.");
+    }
+
+    public void druckeObjekt() {
+        System.out.println("Der Student heisst " + this.name + ", ist im " + this.semester + ". Sem. und hat " + this.bafoegSchulden + " EUR Schulden wegen Bafoeg.");
+    }
+
+    public void druckeObjekt(boolean kurz){
+        if (kurz){
+            System.out.println("Der Student heisst " + this.name + " (" + this.semester + ". Sem.)");
+        } else {
+            druckeObjekt();
+        }
+    }
+
+    public int getSemester() {
+        return this.semester;
+    }
+
+    public void klausurSchreiben() {
+        String dh = "";
+        if (this instanceof DHStudent) {
+            dh = "DH-";
+        }
+
+        System.out.println("Ich bin ein "+dh+"Student namens "+ this.name + " und schreibe meine Klausuren perfekt!");
+    }
 }
