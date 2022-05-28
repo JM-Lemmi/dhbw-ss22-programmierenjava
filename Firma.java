e4534f7cebfe9c68e2f5d2b11c834bf80f67ae87
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2022-05-28 12:17:45 +0200

    Aufgabe 3.2 Klasse "Firma"

--- Firma.java
+++ Firma.java
@@ -1,3 +1,11 @@
 public class Firma {
-    
+    private String name;
+
+    public Firma(String name) {
+        this.name = name;
+    }
+
+    public void druckeName() {
+        System.out.println("Der Firmenname lautet: " + name);
+    }
 }
