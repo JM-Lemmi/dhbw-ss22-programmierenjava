2ca4a7e641c1e1d6568c74036ee9177141689e1b
Julian Lemmerich <41118534+JM-Lemmi@users.noreply.github.com>
2022-05-28 13:52:55 +0200

    Aufgabe 3.1 Lehrbeauftragter

--- Lehrbeauftragter.java
+++ Lehrbeauftragter.java
@@ -1,3 +1,26 @@
 public class Lehrbeauftragter {
-    
+    private String name;
+    private IKlausurSchreiber[] prueflinge;
+
+    public Lehrbeauftragter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrueflinge(IKlausurSchreiber[] prueflinge) {
+        this.prueflinge = prueflinge;
+    }
+
+    public void lasseKlausurSchreiben() {
+        for (IKlausurSchreiber pruefling : prueflinge) {
+            pruefling.klausurSchreiben();
+        }
+    }
+
+    public String toString() {
+        return "";
+    }
 }
